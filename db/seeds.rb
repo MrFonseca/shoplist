401c559c7a9ce62641be3944b61a4d9a266fd4e6
MrFonseca <fredidf@gmail.com>
2017-01-06 08:21:17 -0200

    inserido o check nos items da list

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,6 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+carnes = Category.create(name: 'carnes')
+Item.create(name: 'guisado', category_id: carnes.id)
